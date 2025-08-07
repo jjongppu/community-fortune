cf9feb6914d8b70e391d09dbe3b9ee84c8067718
jonggil.kim <jonggil.kim@dkbmc.com>
2025-08-07 09:04:35 +0900

    코드수정

--- assets/javascripts/discourse/components/community-fortune.js
+++ assets/javascripts/discourse/components/community-fortune.js
@@ -45,8 +45,8 @@ class CommunityFortune extends Component {
 export default setComponentTemplate(
   hbs`
     <div class="fortune-cookie-container {{if this.opened \"opened\"}}">
-      <img src={{get-url "/plugins/community-fortune/images/cookie-left.png"}} class="cookie cookie-left" alt="left cookie" />
-      <img src={{get-url "/plugins/community-fortune/images/cookie-right.png"}} class="cookie cookie-right" alt="right cookie" />
+      <img src="/plugins/community-fortune/images/cookie-left.png" class="cookie cookie-left" alt="left cookie" />
+      <img src="/plugins/community-fortune/images/cookie-right.png" class="cookie cookie-right" alt="right cookie" />
 
       <div
         class="paper-strip"
