d126d581204a3c39444a162fb4ae93d29be19b29
jonggil.kim home <jonggil.kim@dkbmc.com>
2025-08-07 22:52:22 +0900

    modif

--- assets/javascripts/discourse/components/community-fortune.js
+++ assets/javascripts/discourse/components/community-fortune.js
@@ -65,21 +65,66 @@ class CommunityFortune extends Component {
       });
     });
   }
+
+
+  @action
+  checkFortuneVisibility() {
+    try {
+      const expiry = parseInt(localStorage.getItem("hideFortune") || "0", 10);
+      const now = Date.now();
+
+      if (!expiry || now > expiry) {
+        localStorage.removeItem("hideFortune");
+        this.hidden = false;
+      } else {
+        this.hidden = true;
+      }
+    } catch (e) {
+      console.error("쿠키 표시 여부 체크 중 에러:", e);
+      this.hidden = false;
+    }
+  }
+
+
+  @action
+  setupDismissButton() {
+    try {
+      if (confirm("오늘 하루동안 운세를 안 보시겠어요?")) {
+        const expire = new Date();
+        expire.setHours(24, 0, 0, 0);
+
+        localStorage.setItem("hideFortune", expire.getTime().toString());
+        this.hidden = true;
+      }
+    } catch (e) {
+      console.error("닫기 처리 중 에러:", e);
+    }
+  }
+
+
+
 }
+
 
 export default setComponentTemplate(
   hbs`
-    <div class="fortune-cookie-container {{if this.opened \"opened\"}}">
-      <div class="cookie-container">
-        <img {{on "click" this.openCookie}} src={{get-url "/plugins/community-fortune/images/cookie-left.png"}} class="cookie cookie-left" alt="left cookie" />
+  <div class="fortune-plugin-container" {{did-insert this.checkFortuneVisibility}}>
+      <div class="fortune-cookie-container {{if this.opened \"opened\"}} {{if this.hidden \"hidden\"}}">
+        <div class="cookie-container">
+          <img {{on "click" this.openCookie}} src={{get-url "/plugins/community-fortune/images/cookie-left.png"}} class="cookie cookie-left" alt="left cookie" />
 
-        <div class="paper-strip paper-background">
-          {{#if this.opened}}
-            <span class="fortune-text">{{this.fortune}}</span>
-          {{/if}}
+          <div class="paper-strip paper-background">
+            {{#if this.opened}}
+              <span class="fortune-text">{{this.fortune}}</span>
+            {{/if}}
+          </div>
+
+          <img {{on "click" this.openCookie}} src={{get-url "/plugins/community-fortune/images/cookie-right.png"}} class="cookie cookie-right" alt="right cookie" />
         </div>
 
-        <img {{on "click" this.openCookie}} src={{get-url "/plugins/community-fortune/images/cookie-right.png"}} class="cookie cookie-right" alt="right cookie" />
+        <div class="fortune-dismiss-btn" {{on "click" this.setupDismissButton}}>
+          ✕
+        </div>
       </div>
     </div>
   `,
